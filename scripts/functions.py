595276014f945ec75bc2ec5649b0e9e02a4966cb
Raphael <raphaelpeer@hotmail.com>
2018-08-02 18:24:42 +0200

    random forest model (4 features)

--- scripts/functions.py
+++ scripts/functions.py
@@ -10,7 +10,27 @@ import numpy as np
 import pandas as pd
 
 
-def preprocess_dataset(ds: pd.DataFrame, test_set=False):
+def impute_missing_age_values(ds: pd.DataFrame) -> pd.DataFrame:
+    mean_age = ds.groupby(['Sex', 'Family']).Age.mean().round().astype(int)
+    guessed_age = ds.apply(lambda x: mean_age[x.Sex, x.Family], axis=1)
+    ds.Age = ds.apply(
+            lambda x: guessed_age[x.PassengerId] if np.isnan(x.Age) else x.Age,
+            axis=1
+            )
+    return ds
+
+
+def impute_missing_fare_values(ds: pd.DataFrame) -> pd.DataFrame:
+    mean_fare = ds.groupby(['Pclass']).Fare.mean()
+    guessed_fare = ds.apply(lambda x: mean_fare[x.Pclass], axis=1)
+    ds.Fare = ds.apply(
+            lambda x: guessed_fare[x.PassengerId] if np.isnan(x.Fare) else x.Fare,
+            axis=1
+            )
+    return ds
+
+
+def preprocess_dataset(ds: pd.DataFrame, test_set=False, return_df=False):
     ds.index = ds.PassengerId
     ds = ds.drop(['Name', 'Ticket', 'Cabin'], axis=1)
     
@@ -27,16 +47,14 @@ def preprocess_dataset(ds: pd.DataFrame, test_set=False):
     # make category '3 or more relatives':
     ds.Family = ds.Family.map(lambda x: 3 if x >= 3 else x)
     
-    mean_age = ds.groupby(['Sex', 'Family']).Age.mean().round().astype(int)
-    guessed_age = ds.apply(lambda x: mean_age[x.Sex, x.Family], axis=1)
-    ds.Age = ds.apply(
-            lambda x: guessed_age[x.PassengerId] if np.isnan(x.Age) else x.Age,
-            axis=1
-            )
-
+    ds = impute_missing_age_values(ds)
+    ds = impute_missing_fare_values(ds)  # only necessary for test set
     ds = ds.drop(['SibSp', 'Parch', 'PassengerId'], axis=1)
-    X = np.array(ds.drop('Survived', axis=1, errors='ignore'))
     
+    if return_df:
+        return ds
+        
+    X = np.array(ds.drop('Survived', axis=1, errors='ignore'))
     if test_set:
         return X
     
